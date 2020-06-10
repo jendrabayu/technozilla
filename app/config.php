fbea032f9c49093347932aef7c2d3ba1b033adb2
jendrabayu <jendra455@gmail.com>
2020-06-10 19:43:39 +0700

    update

--- app/config.php
+++ app/config.php
@@ -1,11 +1,20 @@
 <?php
 
+/* Database */
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'pweb_online_shop');
 
-define('IMG_PATH', 'public/images/');
 define('INC_ROOT', dirname(__DIR__));
+
+/* Directory dan Url Gambar*/
+define('IMG_PATH', 'public/images/');
 define('IMG_URL', 'http://localhost/shop/public/images/');
+
+/* 
+Regsitasi Admin
+    true : fitur regsitrasi admin aktif
+    false : fitur regsitrasi admin non-aktif
+*/
 define('ADMIN_REGISTER', true);
